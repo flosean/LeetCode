023fb9aa4a9af489d9f53c3fbf82cf69cd3b9374
Sean <david840709@gmail.com>
2018-06-18 18:36:48 +0800

    Almost complete 5

--- 5/main.c
+++ 5/main.c
@@ -5,18 +5,25 @@
 
 char* longestPalindrome(char* s) {
     int i, j, k, len;
-    int start, maxlen;
+    int maxlen, start;
 
     len = strlen(s);
     bool d_table[len][len];
 
-    // Check length 1
     for ( i = 0 ; i < len ; i ++ )
+        for ( j = 0 ; j < len ; j ++ )
+            d_table[i][j] = false;
+
+    if ( len == 1 )
+        return s;
+    // Check length 1
+    for ( i = 0 ; i < len ; i ++ ) {
         d_table[i][i] = true;
+        start = i;
+        maxlen = 1;
+    }
 
     // Check length 2
-    start = 0;
-    maxlen = 0;
     for ( i = 0 ; i < len - 1 ; i ++ )
         if ( s[i] == s[i+1] ) {
             d_table[i][i+1] = true;
@@ -27,7 +34,7 @@ char* longestPalindrome(char* s) {
     // Check length bigger than 3
     for ( i = 3 ; i <= len ; i ++ )
         for ( j = 0 ; j < len - i + 1 ; j ++ ) {
-            k = j + len - 1;
+            k = j + i - 1;
             if ( s[j] == s[k] && d_table[j+1][k-1] ) {
                 d_table[j][k] = true;
                 if ( i > maxlen ) {
@@ -36,6 +43,8 @@ char* longestPalindrome(char* s) {
                 }
             }
         }
+
+    printf("%d %d\n", start, maxlen);
 
     char *ans;
     ans = malloc(sizeof(char) * 2048);
